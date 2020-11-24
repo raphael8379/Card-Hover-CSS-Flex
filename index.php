f20ea1d6f4172396fbc96575d2868f9377013829
raphael anderson <raphael.8379@hotmail.com>
2020-11-24 11:26:13 -0300

    criado arquivo index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" />
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+<title>Raphael Anderson RA</title>
+</head>
+
+<body>
+				<div class="container">  
+                <div class="card">
+                <h2> A </h2>
+                </div>
+                
+                <div class="card">
+                <h2> B </h2>
+                </div>
+                
+                <div class="card">
+                <h2> C </h2>
+                </div>
+                
+                <div class="card">
+                <h2> D </h2>
+                </div>
+                
+                <div class="card">
+                <h2> E </h2>
+                </div>
+                
+                </div>
+</body>
+</html>
